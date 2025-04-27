7dc1f04e5b4d6fbce8b606f632958c7cc0b320a8
Venumadhava Reddy <venumadhavamule@gmail.com>
2025-04-27 07:48:50 +0530

    Spring-Error-Handling intialised

---
+++ Spring-Error-Handling/src/main/java/com/venu/springerrorhandling/SpringErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.venu.springerrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringErrorHandlingApplication.class, args);
+	}
+
+}
