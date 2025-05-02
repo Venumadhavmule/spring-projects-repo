1056e181615f182566197360a560b64f1b06ba42
Venumadhava Reddy <venumadhavamule@gmail.com>
2025-05-02 21:09:13 +0530

    Logback added for log details confiuguration

--- Spring-Error-Handling/src/main/java/com/venu/springerrorhandling/controller/ProductController.java
+++ Spring-Error-Handling/src/main/java/com/venu/springerrorhandling/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.venu.springerrorhandling.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +18,18 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/products")
 public class ProductController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
 	@PostMapping
 	public String createProduct(@Valid @RequestBody ProductRequestDTO request) {
-		System.out.println(request);
+		logger.info(request.toString());
 		return "Product Created: " + request.getName() + ", Price: " + request.getPrice();
 	}
 
 	@GetMapping("{id}")
 	public String getProductById(@PathVariable int id) {
 		if (id < 1) {
-			System.out.println("Id " + id + " is less than 1");
+			logger.error("Id " + id + " is less than 1");
 			throw new IllegalArgumentException("ID must be greater than zero");
 		} else if (id > 100) {
 			throw new ProductNotFoundException("Product With Id: " + id + " not found");
